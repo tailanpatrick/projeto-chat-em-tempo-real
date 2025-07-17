7f2b8b482199d68aa9096c6581dc290f8e9f43aa
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-07-17 18:07:21 -0300

    frontend com tailwind e typescript configurados

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="bg-red-600 border border-[yellow]">
+			Tailwind está funcionando!
+		</div>
+	);
+}
+
+export default App;
